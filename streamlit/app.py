71a45e7f69528d2db2f1f73913d54b04c2263bd8
Micah Kitsunai <kmicah1794@gmail.com>
2024-06-21 15:38:44 +0800

    ✨ add concat df

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,12 +5,15 @@ import json
 st.title("西夏語料庫")
 st.write("This is a simple corpus of 西夏 language.")
 
-with open('../論語全解.json', 'r', encoding='utf-8') as file:
+# 論語全解
+with open('../test.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 
 
 st.title("Corpus Data Display")
+
+dataframes = []
 
 for item in data['corpus_data']:
     id_value = item['id']
@@ -21,10 +24,23 @@ for item in data['corpus_data']:
     st.write(f"**ID**: {id_value}")
     st.write(f"**Meaning**: {meaning_value}")
     
-    st.dataframe(df.T)
-
+    edited_df = st.data_editor(df.T, use_container_width=True)
+    
+    dataframes.append(df.T.reset_index(drop=True))
+    
     st.write("---")
 
+combined_df = pd.concat(dataframes, axis=1, ignore_index=True)
 
-# edited_df = st.data_editor(df) 
-# st.dataframe(edited_df)
+st.write("**Combined DataFrame**:")
+st.dataframe(combined_df)
+
+
+chinese_all = ''.join(combined_df.iloc[3].astype(str))
+pronounciation_all = ' '.join(combined_df.iloc[2].astype(str))
+
+st.write("**Concatenated 'chinese' row**:")
+st.write(chinese_all)
+
+st.write("**Concatenated 'pronounciation' row**:")
+st.write(pronounciation_all)
